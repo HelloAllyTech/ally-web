d602de71202866dd1924a44e43f7bc3d182fa55c
aarathyKeyvalue <aarathy@keyvalue.systems>
2025-03-05 14:42:39 +0530

    RTK integration

---
+++ apps/ally-helpline-dashboard/src/store/store.ts
@@ -0,0 +1,15 @@
+import { sampleAPI } from '@/api/sampleAPI'
+import { configureStore } from '@reduxjs/toolkit'
+
+export const store = configureStore({
+  reducer: {
+    [sampleAPI.reducerPath]: sampleAPI.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(sampleAPI.middleware),
+})
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch
