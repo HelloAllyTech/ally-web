273bfe3fd4d93f12d0bb2b458b1111e9c3748ba9
Afna <afna@keyvalue.systems>
2025-03-10 15:33:40 +0530

    update svgr plugin

--- apps/ally-helpline-dashboard/vite.config.ts
+++ apps/ally-helpline-dashboard/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-// import svgr from "vite-plugin-svgr";
+import svgr from "vite-plugin-svgr";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
@@ -16,8 +16,8 @@ export default defineConfig(({ mode }) => ({
     ],
   },
   plugins: [
+    svgr(),
     react(),
-    // svgr(),
     mode === "development" &&
     componentTagger(),
   ].filter(Boolean),
