892419b020ff87e19f811fe68e57037afb246b4c
Afna <afna@keyvalue.systems>
2025-06-10 17:20:59 +0530

    Initial commit
    To connect your workspace to Nx Cloud, push your repository
    to your git hosting provider and go to the following URL:
    
    https://cloud.nx.app/connect/aQF1MeeCvu

---
+++ apps/ally-UI-mono/app/app.tsx
@@ -0,0 +1,11 @@
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="@ally-ui-mono/ally-UI-mono" />
+    </div>
+  );
+}
+
+export default App;
