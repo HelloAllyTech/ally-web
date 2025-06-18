6578eaac729dbe4ca4b560926231701163081697
Afna <afna@keyvalue.systems>
2025-06-18 11:59:32 +0530

    Refactor ResourceSearch component and update usage in the main page
    - Replaced ResourceSearchBar with ResourceSearch in the main page for improved functionality.
    - Updated ResourceSearchProps to make onSearch a required prop for better type safety.

--- apps/ally-web/src/app/page.tsx
+++ apps/ally-web/src/app/page.tsx
@@ -1,17 +1,16 @@
 'use client';
-import { ResourceSearchBar } from '@ally-ui-mono/ui-shared';
+import { ResourceSearch } from '@ally-ui-mono/ui-shared';
 import { useRouter } from 'next/navigation';
 
 export default function Index() {
   const router = useRouter();
   const onSearch = (searchTerm: string) => {
-    
     router.push(`/search?q=${encodeURIComponent(searchTerm)}`);
   };
 
   return (
     <main className="w-full min-h-screen flex flex-col justify-center items-center gap-4">
-      <ResourceSearchBar initialValue="" onSearch={onSearch}/>
+      <ResourceSearch initialValue="" onSearch={onSearch}/>
     </main>
   );
 }
