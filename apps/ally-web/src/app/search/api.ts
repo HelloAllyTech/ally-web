195094499c14bb402f63af54309ca57cadf1313f
Afna <afna@keyvalue.systems>
2025-06-20 23:35:11 +0530

    Remove console log for environment variables in fetchReferenceDocuments function

--- apps/ally-web/src/app/search/api.ts
+++ apps/ally-web/src/app/search/api.ts
@@ -4,7 +4,6 @@ const API_VERSION = process.env.NEXT_PUBLIC_API_VERSION;
 export const initialFetchLimit = 10;
 
 const fetchReferenceDocuments = async (query: string, category?: string, limit: number = initialFetchLimit) => {
-  console.log(process.env);
   const response = await fetch(
     `${API_BASE_URL}/api/${API_VERSION}/reference-document/search/public`,
     {
