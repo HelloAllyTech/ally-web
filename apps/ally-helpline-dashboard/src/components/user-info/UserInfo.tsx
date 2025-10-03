213727f1827ac70650787a52018713e3515baf79
Abheda K P <114218012+Abheda-kp@users.noreply.github.com>
2025-10-03 12:34:03 +0530

    fix: Correct UI issues in userinfo (#238)

--- apps/ally-helpline-dashboard/src/components/user-info/UserInfo.tsx
+++ apps/ally-helpline-dashboard/src/components/user-info/UserInfo.tsx
@@ -1,8 +1,5 @@
 import { FC, useEffect, useRef, useState } from "react";
 
-import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
-import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
-
 import { AccountCircle, Arrow, Logout } from "@assets";
 import { User } from "@types";
 
@@ -21,27 +18,30 @@ const UserInfo: FC<{ user?: User; onLogout: () => void }> = ({ user, onLogout })
   }, []);
 
   return (
-    <div className="relative" ref={containerRef}>
+    <div className="relative w-full" ref={containerRef}>
       <div
         onClick={() => setShowLogout(prev => !prev)}
-        className="flex border-gray-200 w-[calc(100%-30px)] 
-          py-[20px] px-[5px] mx-[10px] gap-[10px] items-center 
-          cursor-pointer"
+        className="flex border-gray-200 
+      px-3 py-2 items-center cursor-pointer"
       >
-        <AccountCircle className="w-[30px] h-[30px]" />
-        <div className="flex flex-col font-['IBM_Plex_Serif']">
-          <div className="text-[16px] text-gray-800">{user?.name}</div>
-          <div className="text-[12px] text-gray-500">{user?.email}</div>
+        <div className="flex gap-2 items-center">
+          <AccountCircle className="w-[30px] h-[30px] flex-shrink-0" />
+          <div className="flex flex-col font-['IBM_Plex_Serif']">
+            <div className="text-[16px] text-gray-800 ">{user?.name}</div>
+            <div className="text-[12px] text-gray-500 ">{user?.email}</div>
+          </div>
         </div>
-        {showLogout ? (
-          <Arrow className="w-5 h-2 rotate-180 text-gray-600 transform transition-transform ease-in-out duration-300" />
-        ) : (
-          <Arrow className="w-5 h-2  text-gray-600 transition-transform duration-300" />
-        )}
+        <div className="flex-shrink-0">
+          {showLogout ? (
+            <Arrow className="w-5 h-2 -rotate-90 text-gray-600 transform transition-transform ease-in-out duration-300" />
+          ) : (
+            <Arrow className="w-5 h-2 text-gray-600 transition-transform duration-300" />
+          )}
+        </div>
       </div>
 
       {showLogout && (
-        <div className="absolute right-[-10px] top-[-50px] bg-white border shadow-md rounded-md p-2 w-[240px]">
+        <div className="absolute top-2 left-full bg-white border shadow-md rounded-md p-2 w-[240px]">
           <button
             onClick={onLogout}
             className="flex items-center gap-2 text-gray-700 hover:bg-gray-100 py-1 px-2 rounded justify-start w-full"
