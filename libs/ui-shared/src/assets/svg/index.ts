a4989682144946c089e4ee49f08d7f3c4a78e858
abhay-keyvalue <abhay@keyvalue.systems>
2026-01-05 13:53:30 +0530

    feat: Introduce GoogleSignInButton component and refactor Login page to utilize it for improved Google sign-in experience

---
+++ libs/ui-shared/src/assets/svg/index.ts
@@ -0,0 +1,29 @@
+import Focus from "./focus.svg?react";
+import Google from "./google.svg?react";
+import MicOff from "./micOff.svg?react";
+import MicOffWhite from "./micOffWhite.svg?react";
+import MicOn from "./micOn.svg?react";
+import Pause from "./pause.svg?react";
+import Play from "./play.svg?react";
+import PrivacyTip from "./privacyTip.svg?react";
+import Stop from "./stop.svg?react";
+import UserIcon from "./user.svg?react";
+import VolumeMute from "./volumeMute.svg?react";
+import VolumeUp from "./volumeUp.svg?react";
+import Warning from "./warning.svg?react";
+
+export {
+  Focus,
+  MicOff,
+  MicOffWhite,
+  MicOn,
+  Pause,
+  Play,
+  PrivacyTip,
+  Stop,
+  UserIcon,
+  VolumeMute,
+  VolumeUp,
+  Warning,
+  Google,
+};
