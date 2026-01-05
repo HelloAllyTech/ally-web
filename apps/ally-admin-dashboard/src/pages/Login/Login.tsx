1c92fd7f90997c48c75e0f4e70aa0c2fb6477c65
abhay-keyvalue <abhay@keyvalue.systems>
2026-01-05 19:03:16 +0530

    refactor: Clean up imports in Login components for ally-admin and ally-helpline dashboards; improve code organization and maintainability

--- apps/ally-admin-dashboard/src/pages/Login/Login.tsx
+++ apps/ally-admin-dashboard/src/pages/Login/Login.tsx
@@ -1,5 +1,11 @@
 import React, { useState, useEffect, useCallback } from "react";
 
+import { AnimatePresence, motion } from "framer-motion";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { toast } from "sonner";
+
+import { CustomImage, FEATURE_FLAGS_MAP, GoogleSignInButton } from "@ally-ui-mono/ui-shared";
 import { useGenerateOTPMutation, useVerifyOTPMutation, useGoogleSignInMutation } from "@api";
 import { BackCircle, LoginImage } from "@assets";
 import { Button, OTP, TextField } from "@components";
@@ -14,12 +20,6 @@ import {
 import { useUser } from "@hooks";
 import { RootState } from "@store";
 import { validateEmail, openLinkInNewTab } from "@utils";
-import { AnimatePresence, motion } from "framer-motion";
-import { useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
-import { toast } from "sonner";
-
-import { CustomImage, FEATURE_FLAGS_MAP, GoogleSignInButton } from "@ally-ui-mono/ui-shared";
 
 const RESEND_CODE_COUNTDOWN = 60; // 60 seconds
 const DEFAULT_EXPIRES_IN = 10; // 10 minutes
