86ac4322280d3f975cbde8ea2782f1ad51eeb1b3
abhay-keyvalue <abhay@keyvalue.systems>
2026-01-06 14:34:58 +0530

    feat: Implement TimeInput component in Cell and update cellTypes; prepare for future usage in SimulationCreator

---
+++ apps/ally-admin-dashboard/src/components/notion-table/utils.ts
@@ -0,0 +1,24 @@
+export function shortId() {
+  return "_" + Math.random().toString(36).substr(2, 9);
+}
+
+export const cellTypes = {
+  editableText: "editableText",
+  dropdown: "dropdown",
+  dropdownSearchable: "dropdownSearchable",
+  number: "number",
+  select: "select",
+  switch: "switch",
+  emoji_select: "emoji_select",
+  normalText: "normalText",
+  triggerConditions: "triggerConditions",
+  timeInput: "timeInput",
+};
+
+export const keyCodes = {
+  enter: "Enter",
+  escape: "Escape",
+  arrowUp: "ArrowUp",
+  arrowDown: "ArrowDown",
+  mousedown: "mousedown",
+};
