3e93385cc95996c7d7502b9a647432660a78df7e
Shubham Bhoite <shubhambhoite502@gmail.com>
2026-01-19 15:32:58 +0530

    feat: Add visual progress bar on simulation call

--- apps/ally-admin-dashboard/src/constants/SimulationCreator.ts
+++ apps/ally-admin-dashboard/src/constants/SimulationCreator.ts
@@ -399,7 +399,7 @@ export const SIMULATION_CREATOR_FIELD_GROUPS: CreatorFieldGroups[] = [
         dependsOn: "timerMode",
         visibleWhen: (formValues: any) => formValues.timerMode === true,
         defaultValue: "00:10:00",
-        note: "Range 00:00:01 - 01:30:00 (1 second to 90 minutes)",
+        note: "Range 00:00:01 - 01:30:00",
       },
     ] as FormFieldConfig[],
   },
