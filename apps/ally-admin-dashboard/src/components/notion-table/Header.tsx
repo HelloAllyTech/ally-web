696f672af4c547e9fd1e379d39b671924f9d9cf5
aarathyKeyvalue <aarathy@keyvalue.systems>
2026-02-17 18:06:31 +0530

    fix: Enhance CreateBadgeSidePanel and UserBadges components with publish and delete confirmation popups, improve button handling, and update UI elements for better user experience

---
+++ apps/ally-admin-dashboard/src/components/notion-table/Header.tsx
@@ -0,0 +1,26 @@
+import { HeaderProps } from "./types";
+
+export const Header = ({
+  column: { label, getResizerProps, getHeaderProps, hasResizer },
+}: HeaderProps) => {
+  const headerProps = getHeaderProps();
+  const { key, ...restHeaderProps } = headerProps;
+
+  return (
+    <div
+      key={key}
+      {...restHeaderProps}
+      className={`relative w-full h-full bg-white border-r border-border-light select-none`}
+    >
+      <div className="flex items-center p-3 cursor-pointer hover:bg-neutral-100 w-full">
+        <span className="font-medium text-typography-800 truncate">{label}</span>
+      </div>
+      {hasResizer && (
+        <div
+          {...getResizerProps()}
+          className="absolute right-0 top-0 w-1 h-full cursor-col-resize hover:bg-primary-300"
+        />
+      )}
+    </div>
+  );
+};
