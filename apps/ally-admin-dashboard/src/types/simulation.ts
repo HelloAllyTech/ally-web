13693f4c139e5cc1f8fb858639c8153cb5627cb6
Mayur Maskar <141343927+maxyur077@users.noreply.github.com>
2026-03-10 19:18:30 +0530

    Feat/Add Toggle button on the custom fields
    * Added Toggle button on the custom fields
    
    * feat:changed name isEnabled to useInDefaultPrompt
    
    * feat:used existing toggle button component

--- apps/ally-admin-dashboard/src/types/simulation.ts
+++ apps/ally-admin-dashboard/src/types/simulation.ts
@@ -111,6 +111,7 @@ export interface CustomFieldType {
   id?: string;
   name?: string;
   value?: string;
+  useInDefaultPrompt?: boolean;
 }
 export interface terminationEvent {
   eventId: string;
