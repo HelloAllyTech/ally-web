4e8d6888f2dff1cf6fb34aa0e1bafe47b3869409
shubhambhoite502 <shubhambhoite502@gmail.com>
2026-03-19 20:19:40 +0530

    fix: Simulation edit restriction for multi tenant id

--- apps/ally-admin-dashboard/src/components/simulation-list/SimulationList.tsx
+++ apps/ally-admin-dashboard/src/components/simulation-list/SimulationList.tsx
@@ -96,8 +96,8 @@ export const SimulationList: React.FC<SimulationListProps> = ({
 
   const isCreatorOrSuperAdmin = (simulation: Simulation) => {
     if (isSuperAdmin) return true;
-    const createdBy = simulation.createdBy?.toLowerCase();
-    return createdBy === String(currentUser?.id) || createdBy === String(currentUser?.userId);
+    const createdBy = simulation.createdByUserId;
+    return createdBy === currentUser?.id;
   };
 
   // Define columns configuration
